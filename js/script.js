06efc211a1ebd0e682fd08df15e9e5a75a7aefda
estepska <e_domszy@tlen.pl>
2020-09-08 13:11:54 +0200

    Add new fucntion

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
 "use strict";
-document.getElementById("test-button").addEventListener("click", function () {
-  const links = document.querySelectorAll(".titles a");
-  console.log("links:", links);
-});
+
+const titleClickHandler = function (event) {
+  const clickedElement = this;
+  console.log("Link was clicked!");
+
+  /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll(".titles a.active");
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove("a.active");
+  }
+
+  /* add class 'active' to the clicked link */
+  clickedElement.classList.add("active");
+  console.log("clickedElement:", clickedElement);
+
+  /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll("active");
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove("active");
+  }
+
+  /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute("href");
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector("href");
+
+  /* add class 'active' to the correct article */
+  article.classList.add("active");
+};
+
+const links = document.querySelectorAll(".titles a");
+
+for (let link of links) {
+  link.addEventListener("click", titleClickHandler);
+}
