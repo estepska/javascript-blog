8dd2b9434085879af052122b60d22c86f2d7c4e2
estepska <e_domszy@tlen.pl>
2020-09-11 17:58:50 +0200

    Add new function

--- js/script.js
+++ js/script.js
@@ -31,7 +31,48 @@ const titleClickHandler = function (event) {
 
   /* add class 'active' to the correct article */
   article.classList.add("active");
+
+  const optArticleSelector = ".post",
+    optTitleSelector = ".post-title",
+    optTitleListSelector = ".titles";
+
+  function generateTitleLinks() {
+    /* remove contents of titleList */
+
+    const titleList = querySelector.optTitleListSelector();
+    function clearMessages() {
+      document.getElementById("messages").innerHTML = "opiTitleListSelector";
+    }
+    /* find all the articles and save them to variable: articles */
+    const articles = document.optArticleSelector(".post");
+
+    let html = "article";
+    for (let article of articles) {
+      /* get the article id */
+      const articleId = document.optArticleSelector(href);
+
+      /* find the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+      /* get the title from the title element */
+
+      /* create HTML of the link */
+      const linkHTML =
+        '<li><a href="#' +
+        articleId +
+        '"><span>' +
+        articleTitle +
+        "</span></a></li>";
+
+      /* insert link into html variable */
+      html = html + linkHTML;
+    }
+
+    titleList.innerHTML = html;
+  }
 };
+
+generateTitleLinks();
 
 const links = document.querySelectorAll(".titles a");
 
